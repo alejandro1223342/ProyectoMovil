3f9c6c8cac3172472dd4eb8a1cbc58ac51aa2145
Alejandro Muñoz <alejomuoss@gmail.com>
2023-05-22 23:21:19 -0500

    Se habilitaron los metodos suma

--- app/src/main/java/com/example/proyectomovil/SumaActivity.java
+++ app/src/main/java/com/example/proyectomovil/SumaActivity.java
@@ -3,6 +3,9 @@ package com.example.proyectomovil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SumaActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class SumaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suma);
+    }
+
+    public void regresarMenu(View view){
+        this.finish();
+    }
+
+    public void Sumar(View view){
+
+        EditText num1= (EditText) findViewById(R.id.tpnum1);
+        EditText num2= (EditText) findViewById(R.id.tpnum2);
+        TextView total=findViewById(R.id.txtresultado);
+
+        double resultado=Double.parseDouble(num1.getText().toString())+Double.parseDouble(num2.getText().toString());
+
+
+        total.setText(resultado+"");
+
     }
 }
