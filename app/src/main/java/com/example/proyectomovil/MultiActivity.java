6016b0977bc69497498afa1664f66890b028942e
Alejandro Muñoz <alejomuoss@gmail.com>
2023-05-24 22:26:53 -0500

    Se agrega la clase RestaActivity

---
+++ app/src/main/java/com/example/proyectomovil/MultiActivity.java
@@ -0,0 +1,4 @@
+package com.example.proyectomovil;
+
+public class MultiActivity {
+}
---
+++ app/src/main/java/com/example/proyectomovil/RestaActivity.java
@@ -0,0 +1,35 @@
+package com.example.proyectomovil;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RestaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suma);
+    }
+
+
+    public void regresarMenu(View view){
+        this.finish();
+    }
+
+    public void Restar(View view){
+
+        EditText num1= (EditText) findViewById(R.id.tpnum1);
+        EditText num2= (EditText) findViewById(R.id.tpnum2);
+        TextView total=findViewById(R.id.txtresultado);
+
+        double resultado=Double.parseDouble(num1.getText().toString())-Double.parseDouble(num2.getText().toString());
+
+
+        total.setText(resultado+"");
+
+    }
+}
