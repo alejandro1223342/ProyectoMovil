0ad32c56c13b1ba3d7db2ad7b1cc1d5ed8d7edf9
Alejandro Muñoz <alejomuoss@gmail.com>
2023-05-24 23:10:10 -0500

    Se agrego los botones restantes al activity main y se le dio sus respectivas funciones

--- app/src/main/java/com/example/proyectomovil/MainActivity.java
+++ app/src/main/java/com/example/proyectomovil/MainActivity.java
@@ -20,11 +20,39 @@ public class MainActivity extends AppCompatActivity {
     public void irSuma(View view){
         //Indica que actividad va a realizar
         Intent intent = new Intent(this, SumaActivity.class);
-        Intent intent1 = new Intent(this, RestaActivity.class);
-        Intent intent2 = new Intent(this, MultiActivity.class);
-        Intent intent3 = new Intent(this, DivisionActivity.class);
+
         //Ciclo de vida de la actividad
         view.getContext().startActivity(intent);
+
+
+    }
+
+    public void irResta(View view){
+        //Indica que actividad va a realizar
+        Intent intent1 = new Intent(this, RestaActivity.class);
+
+        //Ciclo de vida de la actividad
+        view.getContext().startActivity(intent1);
+
+
+    }
+
+    public void irMulti(View view){
+        //Indica que actividad va a realizar
+
+        Intent intent2 = new Intent(this, MultiActivity.class);
+        //Ciclo de vida de la actividad
+        view.getContext().startActivity(intent2);
+
+
+    }
+
+    public void irDivision(View view){
+        //Indica que actividad va a realizar
+
+        Intent intent3 = new Intent(this, DivisionActivity.class);
+        //Ciclo de vida de la actividad
+        view.getContext().startActivity(intent3);
 
 
     }
