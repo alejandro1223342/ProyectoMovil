cb79048fb5ae15c75acacf936869c284efde6f10
Alejandro Muñoz <alejomuoss@gmail.com>
2023-05-24 22:54:28 -0500

    Se agrega el activity_multi

--- app/src/main/java/com/example/proyectomovil/MultiActivity.java
+++ app/src/main/java/com/example/proyectomovil/MultiActivity.java
@@ -12,7 +12,7 @@ public class MultiActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_multipli);
+        setContentView(R.layout.activity_multi);
     }
 
 
